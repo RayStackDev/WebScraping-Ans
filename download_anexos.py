dbf3fa354f836acc68fb00b0b4cf06fa76b0f276
RayStackDev <raymondlugo64@gmail.com>
2025-12-19 22:32:10 -0400

    Filtrando links de arquivos PDF na página.

--- download_anexos.py
+++ download_anexos.py
@@ -4,9 +4,11 @@ from bs4 import BeautifulSoup
 url = "https://www.gov.br/ans/pt-br/acesso-a-informacao/participacao-da-sociedade/atualizacao-do-rol-de-procedimentos"
 
 resposta = requests.get(url)
-
 soup = BeautifulSoup(resposta.text, "html.parser")
 
 links = soup.find_all("a")
 
-print(f"Total de links encontrados: {len(links)}")
+for link in links[:10]:
+    href = link.get("href")
+    if href and href.lower().endswith:
+        print(href)
