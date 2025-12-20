838805c64e316397716c5ce3be730265dce7baee
RayStackDev <raymondlugo64@gmail.com>
2025-12-19 22:18:47 -0400

    Parseando HTML com BeautifulSoup e lista links da página

--- download_anexos.py
+++ download_anexos.py
@@ -1,7 +1,12 @@
 import requests
+from bs4 import BeautifulSoup
 
 url = "https://www.gov.br/ans/pt-br/acesso-a-informacao/participacao-da-sociedade/atualizacao-do-rol-de-procedimentos"
 
 resposta = requests.get(url)
 
-print(resposta.text[:1000])
+soup = BeautifulSoup(resposta.text, "html.parser")
+
+links = soup.find_all("a")
+
+print(f"Total de links encontrados: {len(links)}")
